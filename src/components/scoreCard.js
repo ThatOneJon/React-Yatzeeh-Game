e162ba1774b0253540b447c7277f7698ffea2ddc
mortal Wombat <83345498+ThatOneJon@users.noreply.github.com>
2022-10-10 09:46:04 +0200

    score card 1.0

--- src/components/scoreCard.js
+++ src/components/scoreCard.js
@@ -9,6 +9,18 @@ export default function ScoreCard(){
     return(
         <div className = "scoreCards">
             <div className = "scoreCard"> 
+                <h2>1<br></br><i className="bi bi-dice-1"></i></h2> 
+                <hr></hr>
+                <h2>2<br></br><i className="bi bi-dice-2"></i></h2> 
+                <hr></hr>
+                <h2>3<br></br><i className="bi bi-dice-3"></i></h2> 
+                <hr></hr>
+                <h2>4<br></br><i className="bi bi-dice-4"></i></h2> 
+                <hr></hr>
+                <h2>5<br></br><i className="bi bi-dice-5"></i></h2> 
+                <hr></hr>
+                <h2>6<br></br><i className="bi bi-dice-6"></i></h2> 
+                <hr></hr>
             </div>
             <div className = "scoreCard"> 
             </div>
